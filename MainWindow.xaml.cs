26ca1b5bb6c1bd7b25583cc5ce5bb1c3aee1f33d
Risen12 <den2033@bk.ru>
2022-12-17 23:30:27 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace CyberClub
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenuButtons.Visibility = Visibility.Visible;
+            TeamButtons.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainMenuButtons.Visibility = Visibility.Collapsed;
+            TeamButtons.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ Team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberClub
+{
+    public class Team
+    {
+        public List<string> Players { get; set; }
+        public string Name;
+        private int id;
+        private int rate;
+
+        public Team(string name, int rating)
+        {
+            Name = name;
+            rate = rating;
+        }
+
+        
+
+    }
+}
